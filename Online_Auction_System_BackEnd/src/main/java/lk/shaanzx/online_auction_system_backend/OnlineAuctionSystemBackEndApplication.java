618887caba7eddb2b703f8e9c6d4533dda645e5f
Shaanzx <anuhaslakshan07@gmail.com>
2025-03-08 15:30:04 +0530

    Project Initialized

---
+++ Online_Auction_System_BackEnd/src/main/java/lk/shaanzx/online_auction_system_backend/OnlineAuctionSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.shaanzx.online_auction_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAuctionSystemBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineAuctionSystemBackEndApplication.class, args);
+    }
+
+}
---
+++ Online_Auction_System_BackEnd/src/test/java/lk/shaanzx/online_auction_system_backend/OnlineAuctionSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.shaanzx.online_auction_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAuctionSystemBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
